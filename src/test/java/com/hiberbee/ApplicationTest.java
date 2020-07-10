289953b3e8c1124ca1883c52d54a26621eeedb26
Vlad Volkov <vlad@hiberbee.com>
2020-07-10 21:30:02 +0300

    Fixed failed run

--- src/test/java/com/hiberbee/ApplicationTest.java
+++ src/test/java/com/hiberbee/ApplicationTest.java
@@ -1,10 +1,38 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Hiberbee
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.hiberbee;
 
 import com.hiberbee.configurations.ApplicationConfiguration;
 import io.cucumber.junit.platform.engine.Cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 
 @CucumberContextConfiguration
 @Cucumber
@@ -13,6 +41,10 @@ import org.springframework.boot.test.context.SpringBootTest;
     webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class ApplicationTest {
 
+  @Autowired private CacheManager cacheManager;
+
   @Test
-  void testMain() {}
+  void testMain() {
+    Assertions.assertThat(this.cacheManager).isInstanceOf(ConcurrentMapCacheManager.class);
+  }
 }
