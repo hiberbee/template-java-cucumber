cb4f24f758977b954d44cd488cb38d7cf4e6264c
Vlad Volkov <vlad@hiberbee.com>
2020-07-10 19:20:53 +0300

    Added sonarcloud scan workflow

--- src/test/java/com/hiberbee/ApplicationTest.java
+++ src/test/java/com/hiberbee/ApplicationTest.java
@@ -1,6 +1,7 @@
 package com.hiberbee;
 
 import com.hiberbee.configurations.ApplicationConfiguration;
+import io.cucumber.junit.platform.engine.Cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @CucumberContextConfiguration
 @EnableCaching
+@Cucumber
 @SpringBootTest(classes = ApplicationConfiguration.class)
 public class ApplicationTest {
 
